87ee8ce9432d30bdc52587d6fc333a066ee54088
Kyle Nelson <kyle.nelson@virtual7.de>
2020-12-03 13:58:48 +0100

    kubernetes build files

---
+++ backend/App.java
@@ -0,0 +1,15 @@
+package com.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(
+          new Object[] { App.class }, args
+        );
+    }
+}
---
+++ scripts/buildfrontendImage.sh
@@ -0,0 +1,2 @@
+docker build --no-cache -t v1-sophia/ ../.
+read -p "Press any key..."
